afedcc34ed3cdba52e8f309c9822b2266fc7ebab
Aurelien Bach <aurelien.bach@hotmail.fr>
2025-08-05 17:45:04 +0200

    premiers fichiers

---
+++ main.js
@@ -0,0 +1,66 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function roundPercent(v){
+    return Math.round(v * 100, 0);
+}
+
+let doneCount = getRandomInt(10);
+let todoCount = getRandomInt(10) + 1;
+const totalTaskCount = doneCount + todoCount;
+
+let mainPageContent = document.getElementById('MainPageContent');
+console.log(mainPageContent)
+
+const listNames = ['Aujourd\'hui', 'Cette semaine', 'Ce mois-ci', 'Plus tard'];
+for (let listName of listNames){
+    const listDiv = document.createElement('div');
+    listDiv.classList.add('MainItemList');
+    mainPageContent.appendChild(listDiv);
+
+    const listDivTitle = document.createElement('div');
+    listDivTitle.textContent = listName;
+    listDiv.appendChild(listDivTitle);
+
+    const listStatsDiv = document.createElement('div');
+    listStatsDiv.classList.add('progress-wrapper');
+    listDiv.appendChild(listStatsDiv);
+
+    const taskCountDone = document.createElement('div');
+    taskCountDone.classList.add('task-count-done');
+    listStatsDiv.appendChild(taskCountDone);
+    taskCountDone.textContent = doneCount;
+
+    const statsProgressBar = document.createElement('div');
+    statsProgressBar.classList.add('progress-bar');
+    listStatsDiv.appendChild(statsProgressBar);
+
+    const statsDoneDiv = document.createElement('div');
+    statsDoneDiv.classList.add('progress-done');
+    statsProgressBar.appendChild(statsDoneDiv);
+    statsDoneDiv.style.width = roundPercent(doneCount/(doneCount + todoCount)) + "%";
+
+    const taskCountTod = document.createElement('div');
+    taskCountTod.classList.add('task-count-todo');
+    listStatsDiv.appendChild(taskCountTod);
+    taskCountTod.textContent = todoCount;
+
+    statsProgressBar.addEventListener('click', (e) => {
+        if (doneCount === totalTaskCount || todoCount === 0)
+            return;
+
+        doneCount += 1;
+        todoCount -= 1;
+        console.log(doneCount, todoCount);
+
+        statsDoneDiv.style.width = `${roundPercent(doneCount/(doneCount + todoCount))}%`;
+        taskCountDone.textContent = `${doneCount}`;
+        taskCountTod.textContent = `${todoCount}`;
+    });
+
+    for (let i = 0; i < 3; i++){
+        const itemDiv = document.createElement('div');
+        listDiv.appendChild(itemDiv);
+    }
+}
